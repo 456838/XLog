e57229866cfa81a0701d9751efaf3c558c3143ec
wujinsheng <jinsheng.wu2@zhenai.com>
2018-11-15 13:40:56 +0800

    clean

---
+++ sample/src/main/java/com/salton123/xlog/demo/XLogSampleApplication.java
@@ -0,0 +1,12 @@
+package com.salton123.xlog.demo;
+
+import android.app.Application;
+
+/**
+ * User: newSalton@outlook.com
+ * Date: 2018/11/15 11:45 AM
+ * ModifyTime: 11:45 AM
+ * Description:
+ */
+public class XLogSampleApplication extends Application {
+}
